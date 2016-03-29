50978a419e701bf12b9887dae14154a60e59a3c1
BalloonWen <whenballoon@gmail.com>
2016-03-29 16:17:24 -0400

    device side app
    view lists from Google Drive

---
+++ PrintPDF/app/src/main/java/com/balloon/printer/printpdf/MainActivity.java
@@ -0,0 +1,107 @@
+package com.balloon.printer.printpdf;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SortOrder;
+import com.google.android.gms.drive.query.SortableField;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private  GoogleApiClient mGoogleApiClient;
+    private ResultsAdapter mResultsAdapter;
+    private ListView mResultsListView;
+    private static final String TAG = "BaseDriveActivity";
+    /**
+     * Request code for auto Google Play Services error resolution.
+     */
+    protected static final int REQUEST_CODE_RESOLUTION = 1;
+
+    /**
+     * Next available request code.
+     */
+    protected static final int NEXT_AVAILABLE_REQUEST_CODE = 2;
+
+    /**
+     * Google API client.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listfiles);
+        mResultsListView = (ListView) findViewById(R.id.listViewResults);
+        mResultsAdapter = new ResultsAdapter(this);
+        mResultsListView.setAdapter(mResultsAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addScope(Drive.SCOPE_APPFOLDER) // required for App Folder sample
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        SortOrder sortOrder = new SortOrder.Builder()
+                .addSortAscending(SortableField.TITLE)
+                .build();
+        Query query = new Query.Builder()
+                .setSortOrder(sortOrder)
+                .build();
+        Drive.DriveApi.query(mGoogleApiClient, query)
+                .setResultCallback(metadataCallback);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (!connectionResult.hasResolution()) {
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, connectionResult.getErrorCode(), 0).show();
+            return;
+        }
+        try {
+            connectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
+        }
+    }
+    final private ResultCallback<DriveApi.MetadataBufferResult> metadataCallback =
+            new ResultCallback<DriveApi.MetadataBufferResult>() {
+                @Override
+                public void onResult(DriveApi.MetadataBufferResult result) {
+                    if (!result.getStatus().isSuccess()) {
+
+                        return;
+                    }
+                    mResultsAdapter.clear();
+                    mResultsAdapter.append(result.getMetadataBuffer());
+                }
+            };
+}
